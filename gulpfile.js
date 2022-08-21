c31a9f079d1ebb9ac42d8a1a33f7f641458792ee
Leon <joaquinleonquero00@gmail.com>
2022-08-21 05:12:46 -0500

    Se añádieron tareas al gulpfile para convertir las imagenes a Webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+// Css
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require("gulp-plumber");
+
+// Imagenes
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
 
 function css(done){
     src('./src/scss/**/*.scss') // Identificar el archivo SASS
@@ -11,11 +17,33 @@ function css(done){
     done();//callback function que avisa a gulp cuando llegamo al final de la function
 }
 
+function imagenes(done){
+    const opciones = {
+        optimmizationLevel: 3,
+    }
+    src("./src/img/**/*.{png,jpg,jpeg}")
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest("./build/img"))
+    done();
+}
+
+function versionWebp(done){
+const opciones = {
+    quality:50
+};
+    src("./src/img/**/*.{png,jpg,jpeg}")
+    .pipe(webp(opciones))
+    .pipe(dest("./build/img"))
+    done();
+}
+
 function dev(done) {
     watch("./src/scss/**/*.scss", css)
     done();
 }
 
-exports.css = css
-exports.dev = dev
+exports.css = css;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes,versionWebp,dev);
 
